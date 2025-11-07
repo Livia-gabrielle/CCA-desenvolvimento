ef7c29bcee50a7e0b6c52eb6ce51641641e35f07
William Calvacante <williamcavalcante801@gmail.com>
2025-11-07 16:21:22 -0500

    Fiz algumas alterações q faltavam q eu tinha esquecido KAAAAAKAAKAK

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './screen/Home/Home'
 import Comunicacao from './screen/Comunicacao/Comunicacao'
 import Sensoriais from './screen/Sensoriais/Sensoriais'
 import Cognitivo from './screen/Cognitivo/Cognitivo'
 import Interacao from './screen/Interacao/Interacao'
 import Motora from './screen/Motora/Motora'
+import Login from './screen/Login/Login';
+import Resgister from './screen/Register/Register';
+
 import NavBar from './layouts/NavBar/NavBar';
+
+
+
 
 function App() {
   return (
@@ -17,9 +24,11 @@ function App() {
           <Route path='/' exect element={<Home />} />
           <Route path='/comunicacao' element={<Comunicacao />} />
           <Route path='/sensoriais' element={<Sensoriais />} />
-          <Route path='cognitivo' element={<Cognitivo />} />
-          <Route path='interacaosocial' element={<Interacao />} />
-          <Route path='motora' element={<Motora />} />
+          <Route path='/cognitivo' element={<Cognitivo />} />
+          <Route path='/interacaosocial' element={<Interacao />} />
+          <Route path='/motora' element={<Motora />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Resgister/>}/>
         </Routes>
       </Router>
     </>
--- src/layouts/NavBar/NavBar.js
+++ src/layouts/NavBar/NavBar.js
@@ -1,20 +1,25 @@
 import styles from './NavBar.module.css'
 import { AiOutlineMenu } from "react-icons/ai";
 import imgLogo from '../../img/LogoCCA.png'
+import { Link } from 'react-router-dom';
 
 
-export default function NavBar(){
-    return(
+export default function NavBar() {
+    return (
         <>
             <header className={styles.header}>
                 <div className={styles.containerHeader}>
-                    <AiOutlineMenu className={styles.iconMenuBar}/>
-                    <div className={styles.containerLogo}>
-                        <img src={imgLogo} alt="logo do CCA" />
-                        <h1>CCA</h1>
+                    <AiOutlineMenu className={styles.iconMenuBar} />
+                    <div className={styles.cardHeader}>
+                        <Link className='link' to='/'>
+                            <div className={styles.containerLogo}>
+                                <img src={imgLogo} alt="logo do CCA" />
+                                <h1>CCA</h1>
+                            </div>
+                        </Link>
                     </div>
-                    <div></div>
                 </div>
+
             </header>
         </>
     )
