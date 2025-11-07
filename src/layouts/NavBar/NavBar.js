f4031130e3e4f0dea6dccc13f80c88fb04e43ec7
Willian Calvacante <williamcavalcante801@gmail.com>
2025-11-07 15:37:33 -0500

    Fiz o layoute do header

--- src/layouts/NavBar/NavBar.js
+++ src/layouts/NavBar/NavBar.js
@@ -1,9 +1,21 @@
 import styles from './NavBar.module.css'
+import { AiOutlineMenu } from "react-icons/ai";
+import imgLogo from '../../img/LogoCCA.png'
+
 
 export default function NavBar(){
     return(
         <>
-            <h3>Aqui NavBar</h3>
+            <header className={styles.header}>
+                <div className={styles.containerHeader}>
+                    <AiOutlineMenu className={styles.iconMenuBar}/>
+                    <div className={styles.containerLogo}>
+                        <img src={imgLogo} alt="logo do CCA" />
+                        <h1>CCA</h1>
+                    </div>
+                    <div></div>
+                </div>
+            </header>
         </>
     )
 }
