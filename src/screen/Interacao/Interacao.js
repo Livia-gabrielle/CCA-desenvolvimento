6149fcaf217b1b2dc7d47950ec76f3201cf028dc
Livia Dias <liviadias120@gmail.com>
2025-11-13 06:38:07 -0500

    Adicionado coisas que relembrei de modificar

--- src/screen/Interacao/Interacao.js
+++ src/screen/Interacao/Interacao.js
@@ -10,7 +10,7 @@ export default function Interacao() {
             <section className={styles.section}>
                 <div className={styles.container}>
                     <div className={styles.card}>
-                        <h2>Comunicacão</h2>
+                        <h2>Interação Social</h2>
                         <p>Estudos em fonoaudiologia e desenvolvimento infantil, como os de Paul e Norbury (2012), indicam que atividades lúdicas e estruturadas ajudam a melhorar linguagem expressiva e receptiva, habilidades sociais e compreensão de sinais sociais. Essas práticas permitem que a criança se expresse, compreenda melhor o mundo ao redor e participe de forma mais ativa das relações sociais.</p>
                         <div><img src={imgInteracao} alt="Mãe ensinando sua filha a interagir com os dedos" /></div>
                         <Link to='/interacao-social/jogo-imitacao'><Button size='large' version='blueBck'>Jogo da Imitação</Button></Link>
